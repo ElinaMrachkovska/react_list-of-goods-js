f823f34a1cdbf0aafbd28f2505e5334d594a2ead
Elina <elina@MacBook-Pro-Elina.local>
2025-10-06 07:58:21 +0300

    develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +15,103 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [sortBy, setSortBy] = useState('');
+  const [goods] = useState(goodsFromServer);
+  const [reversBy, setReversBy] = useState(false);
+  const [selectedGoods, setSelectedGoods] = useState([]);
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  const isDefaultOrder = sortBy === '' && reversBy === false;
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  const handleSetSort = field => {
+    setSortBy(field);
+    setReversBy(false);
+  };
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+  const handleReverseSort = () => {
+    setReversBy(current => !current);
+  };
+
+  const handleResetSort = () => {
+    setSortBy('');
+    setReversBy(false);
+  };
+
+  const handleSelectGoods = good => {
+    setSelectedGoods(currentSelectedGoods => {
+      if (currentSelectedGoods.includes(good)) {
+        return currentSelectedGoods.filter(item => item !== good);
+      }
+
+      return [...currentSelectedGoods, good];
+    });
+  };
+
+  const visibleGoods = [...goods];
+
+  if (sortBy === 'name') {
+    visibleGoods.sort((a, b) => a.localeCompare(b));
+  } else if (sortBy === 'length') {
+    visibleGoods.sort((a, b) => a.length - b.length);
+  }
+
+  if (reversBy) {
+    visibleGoods.reverse();
+  }
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${sortBy === 'name' ? '' : 'is-light'}`}
+          onClick={() => handleSetSort('name')}
+        >
+          Alfa
+        </button>
+
+        <button
+          type="button"
+          className={`button is-info ${sortBy === 'length' ? '' : 'is-light'}`}
+          onClick={() => handleSetSort('length')}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={`button is-warning ${reversBy ? '' : 'is-light'}`}
+          onClick={handleReverseSort}
+        >
+          Reverse
+        </button>
+
+        {!isDefaultOrder && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={handleResetSort}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li key={good}>
+            <button
+              type="button"
+              className={`button is-small ${
+                selectedGoods.includes(good) ? 'is-success' : 'is-light'
+              }`}
+              onClick={() => handleSelectGoods(good)}
+            >
+              {good}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
