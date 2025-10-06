008f8cee6016bd62781c09fc528a9edbafac5ee2
Elina <elina@MacBook-Pro-Elina.local>
2025-10-06 08:11:11 +0300

    develop

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ export const App = () => {
 
       <ul>
         {visibleGoods.map(good => (
-          <li key={good}>
+          <li key={good} data-cy="Good">
             <button
               type="button"
               className={`button is-small ${
